28a85a4153a4ed1abfc5835aca9af32e7c501e42
saragadamraviteja <saragadamraviteja@gmai.com>
2020-05-15 12:41:12 +0530

    todo list using react native

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import TodoList from "./ToDO.js"
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TodoList />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
